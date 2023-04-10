b44e5b0f6ca1d91f3f874ce938a2e4a486cb529f
System Administrator <root@Marinas-MacBook-Pro.local>
2023-04-11 02:10:44 +0300

    init game

--- src/App.js
+++ src/App.js
@@ -1,25 +1,115 @@
-import logo from './logo.svg';
 import './App.css';
+import config from "./config";
+import React from "react";
+import Card from "./components/Card";
+import Popup from "reactjs-popup";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {cards: this.prepareCard(), clicks: 0, isPopupOpened: false}
+    }
+
+    componentDidMount() {
+        this.startGame();
+    }
+
+    startGame() {
+        this.setState({
+            cards: this.prepareCard(), clicks: 0, isPopupOpened: false
+        })
+    }
+
+    prepareCard() {
+        let id = 1;
+        return [...config.cards, ...config.cards]
+            .sort(() => Math.random() - 0.5)
+            .map(item => ({...item, id: id++, isOpened: false, isCompleted: false}))
+    }
+
+    choiceCardHandler(openedItem) {
+        if (openedItem.isCompleted || this.state.cards.filter(item => item.isOpened).length >= 2) {
+            return;
+        }
+        this.setState({
+            cards: this.state.cards.map(item => {
+                return item.id === openedItem.id ? {...item, isOpened: true} : item;
+            })
+        }, () => {
+            this.processChoosingCards();
+        });
+
+
+        this.setState({clicks: this.state.clicks + 1});
+    }
+
+    processChoosingCards() {
+        const openedCards = this.state.cards.filter(item => item.isOpened);
+        if (openedCards.length === 2) {
+            if (openedCards[0].name === openedCards[1].name) {
+                this.setState({
+                    cards: this.state.cards.map(item => {
+                        if (openedCards[0].id === item.id || openedCards[1].id === item.id) {
+                            item.isCompleted = true;
+                        }
+                        item.isOpened = false;
+                        return item;
+
+                    })
+                }, () => {
+                    this.checkForAllCompleted();
+                })
+            } else {
+                setTimeout(() => {
+                    this.setState({
+                        cards: this.state.cards.map(item => {
+                            item.isOpened = false;
+                            return item;
+                        })
+                    })
+                }, 1000)
+
+            }
+        }
+    }
+
+    checkForAllCompleted() {
+        if (this.state.cards.every(item => item.isCompleted)) {
+            this.setState({isPopupOpened: true});
+        }
+    }
+
+    closePopup() {
+        this.setState({isPopupOpened: false});
+        this.startGame();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="header">Memory Game</header>
+                <div className="game">
+                    <div className="score">Нажатий: {this.state.clicks}</div>
+                    <div className="cards">
+                        {
+                            this.state.cards.map(item => (
+                                <Card key={item.id} item={item} isShowed={item.isOpened || item.isCompleted}
+                                      onChoice={this.choiceCardHandler.bind(this)}></Card>
+                            ))
+                        }
+                    </div>
+                </div>
+                <Popup open={this.state.isPopupOpened} closeOnDocumentClick onClose={this.closePopup.bind(this)}>
+                    <div className="modal">
+                        <span className="close" onClick={this.closePopup.bind(this)}>
+                            &times;
+                        </span>
+                        Игра завершена! Ваш результат: {this.state.clicks} кликов!
+                    </div>
+                </Popup>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,25 @@
+import React from "react";
+import './Card.css';
+
+class Card extends React.Component {
+
+    cardClickHandler(item) {
+        this.props.onChoice(item);
+
+    }
+
+    render() {
+        return (<div className={'card ' + (this.props.isShowed ? 'opened' : 'closed')}
+                     onClick={this.cardClickHandler.bind(this, this.props.item)}>
+            <div className="card-inner card-front">
+                <img src={'/images/' + this.props.item.image} alt={this.props.item.name}/>
+            </div>
+            <div className="card-inner card-back">
+                <img src={'/images/question.svg'} alt="question mark"/>
+            </div>
+
+        </div>)
+    }
+}
+
+export default Card;
